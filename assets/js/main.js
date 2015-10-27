4ac45275cdd1361a32460636e2ca026b98a31644
liliionita <lili.ionita@brandfusion.ro>
2015-10-27 19:40:50 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
+function listLimit (elm, line){
+    var maxHeight = parseInt(elm.css('line-Height'))*line;
+
+    while(elm.height() > maxHeight){
+        var text = elm.text();
+        elm.text(text.substring(0,text.length-10)).text(elm.text()+' ...');
+    }
+}
+
 $(document).ready(function() {
 	$('.flexslider').flexslider({
 		animation: "slide"		
 	});
+	$('.item-details').each(function(){
+	   listLimit ($(this), 5)
+	})
 });
